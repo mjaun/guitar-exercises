5509e944e3e33403560613fd3329a950e9a359a3
Mario Jaun <mario.jaun@gmail.com>
2024-11-12 22:35:50 +0100

    update exercises

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ from fretboard import Tuning, Context, get_all_caged_shapes
 from music_theory import Scale
 from output import GuitarProFile, print_shape, print_tab
 
-tuning_text = 'E2-A2-D3-G3-B3-E4'
-scale_text = 'E Aeolian'
 number_of_exercises = 1
+tuning_text = 'E2-A2-D3-G3-B3-E4'
+
+scale_texts = [
+    'E Aeolian',
+    'A Aeolian',
+    'G Aeolian',
+    'D Aeolian',
+]
+
 
 ED = ExerciseDescriptor
 
@@ -30,9 +37,9 @@ melodic_sequences = [
 
 intervals = {
     '3rds*': 2,
-    '4ths*': 3,
+    # '4ths*': 3,
     # '5ths': 4,
-    '6ths*': 5,
+    # '6ths*': 5,
     # '7ths': 6,
     # 'Octaves': 7,
 }
@@ -44,7 +51,7 @@ interval_patterns = [
     # *[ED(f'{n}: One Down, One Up', [-s, 1, s, 1], Feel.STRAIGHT) for n, s in intervals.items()],
     *[ED(f'{n}: Two Up, One Down*', [s, -(s - 1), s, 1, -s, 1], Feel.STRAIGHT) for n, s in intervals.items()],
     # *[ED(f'{n}: Two Down, One Up', [-s, (s + 1), -s, 1, s, 1], Feel.STRAIGHT) for n, s in intervals.items()],
-    *[ED(f'{n}: In Triplets*', [s, -(s - 1)], Feel.TRIPLET) for n, s in intervals.items()],
+    # *[ED(f'{n}: In Triplets*', [s, -(s - 1)], Feel.TRIPLET) for n, s in intervals.items()],
     # *[ED(f'{n}: One Up, One Down, In Triplets*', [s, 1, -s], Feel.TRIPLET) for n, s in intervals.items()],
 ]
 
@@ -59,7 +66,7 @@ triad_patterns = [
     # ED('Triads: Four Note Pattern, Low Note Doubled 2', [4, -2, -2, 1], Feel.STRAIGHT),
     # ED('Triads: Four Note Pattern, Middle Note Doubled 2', [-2, 2, 2, -1], Feel.STRAIGHT),
     ED('Triads: Four Note Pattern, High Note Doubled*', [-4, 2, 2, 1], Feel.STRAIGHT),
-    ED('Triads: Ascending, 3 Against 2 Feel*', [2, 2, -3], Feel.STRAIGHT),
+    # ED('Triads: Ascending, 3 Against 2 Feel*', [2, 2, -3], Feel.STRAIGHT),
     # ED('Triads: Descending, 3 Against 2 Feel', [-2, -2, 5], Feel.STRAIGHT),
     # ED('Triads: Combined, 3 Against 2 Feel', [2, 2, 1, -2, -2, 1], Feel.STRAIGHT),
     # ED('Triads: Reversed Combined, 3 Against 2 Feel', [-2, -2, 1, 2, 2, 1], Feel.STRAIGHT),
@@ -72,9 +79,9 @@ arpeggio_patterns = [
     # ED('Arpeggios: Descend Then Ascend', [-2, -2, -2, 1, 2, 2, 2, 1], Feel.STRAIGHT),
     # ED('Arpeggios: Low To High Then Descend', [6, -2, -2, -1], Feel.STRAIGHT),
     # ED('Arpeggios: Descend Then Jump', [-2, -2, 6, -1], Feel.STRAIGHT),
-    ED('Arpeggios: 4 Against 3 Feel Ascending*', [2, 2, 2, -5], Feel.TRIPLET),
-    ED('Arpeggios: 4 Against 3 Feel Descending*', [-2, -2, -2, 7], Feel.TRIPLET),
-    ED('Arpeggios: 4 Against 3 Feel Ascend Then Descend*', [2, 2, 2, 1, -2, -2, -2, 1], Feel.TRIPLET),
+    # ED('Arpeggios: 4 Against 3 Feel Ascending*', [2, 2, 2, -5], Feel.TRIPLET),
+    # ED('Arpeggios: 4 Against 3 Feel Descending*', [-2, -2, -2, 7], Feel.TRIPLET),
+    # ED('Arpeggios: 4 Against 3 Feel Ascend Then Descend*', [2, 2, 2, 1, -2, -2, -2, 1], Feel.TRIPLET),
     # ED('Arpeggios: 4 Against 3 Feel Descend Then Ascend', [-2, -2, -2, 1, 2, 2, 2, 1], Feel.TRIPLET),
 ]
 
@@ -87,6 +94,7 @@ all_exercises: List[ExerciseDescriptor] = [
 
 
 def main():
+    scale_text = random.choice(scale_texts)
     ctx = Context(Tuning.from_text(tuning_text), Scale.from_text(scale_text))
     caged_position, shape = random.choice(get_all_caged_shapes(ctx))
 
